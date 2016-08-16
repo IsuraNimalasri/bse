129f98e085853491faa132a7541fc7f5b79bdda7
Sasha-P <sasha.pazyuk.com>
2016-08-16 20:35:29 +0300

    css

--- src/static/js/admin.js
+++ src/static/js/admin.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
             processData: false, // Don't process the files
             contentType: false, // Set content type to false as jQuery will tell the server its a query string request
             success: function(data, textStatus, jqXHR) {
-                    if (!('error' in data['results'])) {
+                    if (!('error' in data.results)) {
 
                         // console.log('data: ' + JSON.stringify(data));
 
