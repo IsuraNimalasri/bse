66f0f23f4b1ba70f3e91fa8a1b08ffc81fb9f2ff
Sasha-P <sasha.pazyuk.com>
2016-08-16 20:23:44 +0300

    html alert

--- src/static/js/admin.js
+++ src/static/js/admin.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
             processData: false, // Don't process the files
             contentType: false, // Set content type to false as jQuery will tell the server its a query string request
             success: function(data, textStatus, jqXHR) {
-                    if(typeof data['error'] === 'undefined') {
+                    if (!('error' in data)) {
 
                         // console.log('data: ' + JSON.stringify(data));
 
